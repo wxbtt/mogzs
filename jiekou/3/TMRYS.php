d77214a9f8873434780167762dfbd056c6133e4b
GitHub Action <action@github.com>
2025-07-26 07:46:27 +0000

    Update with latest crawl results (cleaned temp files)

---
+++ jiekou/3/TMRYS.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+<meta name="viewport" content="width=device-width, initial-scale=0.8">
+    <title>TVbox接口加密</title>
+
+</head>
+
+<body>
+<br><br><br><br><br><br>
+<h2>加密TVbox点播源</h2>
+<br><br>
+    <form action="TMR_YS.php" method="post">
+        <label for="mode">模式</label>
+        <select name="mode" id="mode">            
+            <option value="encrypt">加密</option>
+        	<option value="decrypt">解密</option>
+
+        </select><br><br>
+
+        <label for="data">原文:</label>
+        <textarea name="data" id="data" rows="9" cols="50" value=""
+></textarea><br><br>
+
+        <label for="password">密码(16位):</label>
+        <input name="password" id="password" value="1234567000000000"><br><br>
+
+        <label for="iv">偏移(16位):</label>
+        <input type="text" name="iv" id="iv" value="1234567891234000"><br><br>
+      <div style="width:410px;text-align:right;"><input type="submit" value="运行！"></div>
+
+  </form>
+<br><br><br><br><br>
+♢使用教程链接：<a href=https://mp.weixin.qq.com/s?__biz=Mzk1NzcwNzQ0MQ==&mid=2247486659&idx=1&sn=aeb93f745d3d38c97ddef2cc2d666868&chksm=c245378efd6503202dd14a35fc080166fab9d5c0e50c5210dd7c704345a387f5e4fc323ea87d&forbs=1&scene=126&subscene=91&sessionid=1737030739&flutter_pos=1&clicktime=1737030773&enterid=1737030773&finder_biz_enter_id=5&ascene=56&fasttmpl_type=0&fasttmpl_fullversion=7561929-zh_CN-zip&fasttmpl_flag=0&realreporttime=1737030773511&devicetype=android-34&version=28003653&nettype=WIFI&abtest_cookie=AAACAA==&lang=zh_CN&session_us=gh_9b1116d07b19&countrycode=CN&exportkey=n_ChQIAhIQZROvZO4bmFd2hQOMF3OEYhLwAQIE97dBBAEAAAAAAIoQDT519PYAAAAOpnltbLcz9gKNyK89dVj0gQ1qo5yjJR1Yyt8itBD5WU4dzgRkN2XXceSRcvgIGoUtB4XAz9/Y1dTQ2okb5WLDw53XebZNldxJ6DarcnV8jIZUxGBT8a5eKzACfkfgvEstLGe2tXXhR5hlqJtl3SM4tB0VD1aEmOgwk2QWU12TC2InORJGzdN899KuRL+oAZgk968J3TNohVL+xVp0JcE3Ell7hHW/uFoTcDselnFSGcPPpj8DSBJq7xk5RD6BBJNiPQGCVLGc0U84gLVgIipC7YFCyLv5ijWxTg==&pass_ticket=LnpPtpKj82PlY3fUXw55zBEbqWyrMeTzwZ9wA0wVrCqpmVzHLd/L3M5F9ILgijnS&wx_header=3 target=_blank>查看教程</a><br>
+*暂时不提供对别人加密源的破解功能。
+
+<br><br><br><br><br><br><br><br><br><br><br><br>
+公众号：天命人影视
+🐾12/6298</body>
+
+</html>
